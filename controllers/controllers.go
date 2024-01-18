dc55706dc7cc7cffd9c1354ed4b753a688f7f3b4
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-18 16:36:21 -0300

    feat: busca por ID de personalidade

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +16,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode(personalidade)
+		}
+	}
 }
